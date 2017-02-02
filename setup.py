d91d1caf5d126d084a08901231a8002a70410857
Peter Inglesby <peter.inglesby@gmail.com>
2017-02-02 21:33:02 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+
+import os
+import re
+
+
+def read(*parts):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(here, *parts)) as f:
+        return f.read()
+
+
+VERSION = re.search(
+    "^__version__ = '(.*)'$",
+    read('src', 'template_flamegraph', '__init__.py'),
+    re.MULTILINE
+).group(1)
+
+if __name__ == '__main__':
+    setup(
+        name='django-debug-toolbar-template-flamegraph',
+        version=VERSION,
+        description='Template flamegraphs for Django Debug Toolbar',
+        long_description=read('README.rst'),
+        packages=find_packages(where='src'),
+        package_dir={'': 'src'},
+        install_requires=['django-debug-toolbar'],
+        url='http://github.com/inglesp/django-debug-toolbar-template-flamegraph',
+        author='Peter Inglesby',
+        author_email='peter.inglesby@gmail.com',
+        license='License :: OSI Approved :: MIT License',
+    )
---
+++ tests/test_template_flamegraph.py
@@ -0,0 +1,5 @@
+import template_flamegraph
+
+
+def test_it():
+    assert template_flamegraph.__version__ == '0.1.0'
